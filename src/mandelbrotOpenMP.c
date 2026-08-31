aefe948ae558862cbdbe90e3ff059254b4ee2861
pedrovcb <pedrovcb@gmail.com>
2026-08-31 01:20:40 -0300

    feat(OpenMP): add mandelbrotOpenMP

---
+++ src/mandelbrotOpenMP.c
@@ -0,0 +1,19 @@
+#include "base.h"
+
+void calcOpenMP(unsigned char *buffer, int largura, int altura, int maxIteracoes, int numThreads) {
+
+    #pragma omp parallel for num_threads(numThreads)
+    
+    for (int py = 0; py < altura; py++) {
+        for (int px = 0; px < largura; px++) {
+
+            double cReal = -2.0 + (double)px / largura * 3.0;
+            double cImag = -1.5 + (double)py / altura * 3.0;
+
+            int iteracoes = calculoMandelbrot(cReal, cImag, maxIteracoes);
+
+            unsigned char intensidade = intensidadePixels(iteracoes, maxIteracoes);
+            buffer[py * largura + px] = intensidade;
+        }
+    }
+}
